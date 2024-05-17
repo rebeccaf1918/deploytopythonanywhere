633157a619ec04e25dd76dcc7538b27c1c7612e2
Rebecca Feeley <G00425652@atu.ie>
2024-05-17 02:08:16 +0100

    adding code to server file for crud operations

--- server.py
+++ server.py
@@ -8,3 +8,37 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+@app.route('/TaylorSwiftSongs', methods=['GET']) #Defining route to retieve all Taylor Swift songs from the database
+def getAll():
+    return songDAO.getall()
+    return jsonify(results) # converts the result from the database to a JSON format
+
+@app.route('/TaylorSwiftSongs/<int:id>', methods=['GET'])
+def findbyID(id):
+    foundSong = songDAO.findbyID(id)
+    return jsonify(foundSong)
+
+@app.route('/TaylorSwiftSongs', methods=['POST'])
+def create():
+    
+    if not request.json: #if the request doesn't have JSON data, it aborts and shows a 400 Bad Request error
+        abort(400) 
+
+    song = {
+        "Title": request.json['Title'],
+        "Album": request.json['Album'],
+        "Genre": request.json['Genre'],
+        "Chart Position": request.json['Chart Position']
+
+    }                     
+
+    addedSong = songDAO.create(song)
+    return jsonify(addedSong)
+       
+
+
+
+

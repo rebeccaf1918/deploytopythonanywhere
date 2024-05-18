7a5e6bd4b39e408b0ae071d100385c2a27191867
Rebecca Feeley <G00425652@atu.ie>
2024-05-18 04:00:03 +0100

    adding functions to DAO to be used in server.py

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, url_for, request, redirect, abort
+import songDAO
+
 
 app = Flask(__name__)
 
@@ -10,11 +12,9 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-@app.route('/rebeccaf1918', methods=['GET']) #Defining route to retieve all Taylor Swift songs from the database
+@app.route('/TaylorSwiftSongs', methods=['GET']) #Defining route to retieve all Taylor Swift songs from the database
 def getAll():
     return 'taylor'
-
-
 
 @app.route('/TaylorSwiftSongs', methods=['GET']) #Defining route to retieve all Taylor Swift songs from the database
 def getAll():
@@ -25,24 +25,5 @@ def getAll():
 def findbyID(id):
     foundSong = songDAO.findbyID(id)
     return jsonify(foundSong)
-
-@app.route('/TaylorSwiftSongs', methods=['POST'])
-def create():
-    
-    if not request.json: #if the request doesn't have JSON data, it aborts and shows a 400 Bad Request error
-        abort(400) 
-
-    song = {
-        "Title": request.json['Title'],
-        "Album": request.json['Album'],
-        "Genre": request.json['Genre'],
-        "Chart Position": request.json['Chart Position']
-
-    }                     
-
-    addedSong = songDAO.create(song)
-    return jsonify(addedSong)
-       
-
 
 
--- songDAO.py
+++ songDAO.py
@@ -8,6 +8,9 @@
 
 import mysql.connector
 import dbconfig as cfg
+from mysql.connector import cursor
+from mysql.connector.errors import Error
+import sys
 
 class songDAO:    # Defining the class for accessing the song data contained in the database &
     connection =""      # initialising the class variables
@@ -37,5 +40,106 @@ class songDAO:    # Defining the class for accessing the song data contained in 
         self.connection.close()
         self.cursor.close()
 
+    def createDBtable(self): #creates the table if it does not already exist
+        try:
+            sql = """CREATE TABLE IF NOT EXISTS TaylorSwiftSongs (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        Title VARCHAR(250),
+                        Album VARCHAR(250),
+                        Genre VARCHAR(250),
+                        Charting int
+                    )"""
+            self.cursor.execute(sql)
+            self.connection.commit()
+        except mysql.connector.Error as e:
+            print(f"Error creating table: {e}")
 
-   
+
+    def getAll(self):
+        cursor = self.getcursor()
+        sql="SELECT * FROM TaylorSwiftSongs" #sql language to get all data from table
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        #print(results)
+        for result in results:
+            returnArray.append(self.convertToDictionary(result))
+        self.closeAll()
+        return returnArray
+    
+    def findByID(self, ID):
+        try:
+            cursor = self.getcursor()
+            sql="SELECT * FROM TaylorSwiftSongs WHERE ID = %s"
+            values = (ID,)
+            cursor.execute(sql, values)
+            result = self.cursor.fetchone()
+            returnvalue = self.convertToDictionary(result)
+            self.closeAll()
+            return returnvalue
+        except mysql.connector.Error as e:
+            print(f"Error finding recipe: {e}")
+            return None
+
+
+    def create(self, SONG):
+        cursor = self.getcursor()
+        sql="INSERT INTO TaylorSwiftSongs (Title, Album, Genre, Charting) values (%s,%s,%s,%s)"
+        values = (SONG.get("Title"), SONG.get("Album"), SONG.get("Genre"), SONG.get("Charting"))
+        cursor.execute(sql, values)
+
+        self.connection.commit()
+        newID = cursor.lastrowid
+        SONG["ID"] = newID
+        self.closeAll()
+        return SONG
+
+
+    def update(self, ID, SONG):
+        cursor = self.getcursor()
+        sql="UPDATE taylorswiftsongs set Title= %s, Album=%s, Genre=%s, Charting=%s where ID = %s"
+        print(f"update TaylorSwiftSongs {SONG}")
+        values = (SONG.get("Title"), SONG.get("Album"), SONG.get("Genre"), SONG.get("Charting"), ID) 
+        cursor.execute(sql, values)
+        self.connection.commit()
+        self.closeAll()
+
+        
+    def delete(self, ID):
+        cursor = self.getcursor()
+        sql="DELETE FROM TaylorSwiftSongs where ID = %s"
+        values = (ID,)
+
+        cursor.execute(sql, values)
+
+        self.connection.commit()
+        self.closeAll()
+        print("Delete Completed")
+
+    
+    
+    
+    def findByGenre(self, Genre):
+        cursor = self.getcursor()
+        sql="SELECT * FROM TaylorSwiftSongs Where Genre = %s"
+        values = (Genre,)
+
+        cursor.execute(sql, values)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            returnArray.append(self.convertToDictionary(result))
+        return returnArray
+    
+    def convertToDictionary(self, resultLine):
+        attkeys=['ID', 'Title', 'Album','Genre', 'Charting']
+        broker = {}
+        currentkey = 0
+        for attrib in resultLine:
+            broker[attkeys[currentkey]] = attrib
+            currentkey = currentkey + 1 
+        return broker
+
+        
+songDAO = songDAO()
